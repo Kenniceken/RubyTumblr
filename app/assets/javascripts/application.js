55a86e32b37fd6a2869624847a4575021d8bf3bc
Kennice Offor <kennice_offor@yahoo.com>
2020-05-03 21:58:57 +0800

    final deploy edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require popper
 //= require tether
 //= require chosen-jquery
 
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
